78c68b4575b7dabcdb65bfbef7436ae9f15f3bf6
majiiddddd <majid.fathi12@gmail.com>
2024-07-05 10:19:40 +0200

    end of project code

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var btn = document.getElementById("button");
+btn.addEventListener('click' , generate)
+
+function generate(){
+    var quotes = { 
+        "Jules Renard" : '"The truly free man is the one who can turn down an invitation to dinner without giving an excuse."', 
+        "Pierce Brown" :  '"Man cannot be freed by the same injustice that enslaved it."', 
+        "Deepak Chopra" : '"Sex is always about emotions. Good sex is about free emotions; bad sex is about blocked emotions."', 
+        "Madeline Miller" : '"Bury us, and mark our names above. Let us be free."'
+    }
+
+    
+    var authors = Object.keys(quotes);
+    
+    var author = authors[Math.floor(Math.random() * authors.length)];
+
+    var quote = quotes[author];
+
+    document.getElementById("quote").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
+} 
+
+
